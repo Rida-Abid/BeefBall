c14e65917ba8298782ee179986cabe7dd37b360e
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-03-03 00:28:44 +0500

    Added objects obstacles at random positions

---
+++ BeefBall/Screens/GameScreen.Event.cs
@@ -0,0 +1,21 @@
+using System;
+using FlatRedBall;
+using FlatRedBall.Input;
+using FlatRedBall.Instructions;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Specialized;
+using FlatRedBall.Audio;
+using FlatRedBall.Screens;
+using BeefBall.Entities;
+using BeefBall.Screens;
+namespace BeefBall.Screens
+{
+    public partial class GameScreen
+    {
+        void OnPlayerBall1VsObstaclesCollided (BeefBall.Entities.PlayerBall playerBall, FlatRedBall.Math.Geometry.ShapeCollection shapeCollection) 
+        {
+            
+        }
+        
+    }
+}
